ffd259532fc171a241e067e7569388f0f0bdfa79
Kulikov Maksim <kulikovman@gmail.com>
2023-03-28 13:38:28 +0500

    Initial commit

---
+++ bborisovich.py
@@ -0,0 +1,23 @@
+import telebot
+import setting
+
+bot = telebot.TeleBot(setting.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def message_reply(message):
+    if message.text.lower() == 'да':
+        bot.send_message(chat_id=message.chat.id, text="Пизда", reply_to_message_id=message.id)
+    elif message.text.lower() == "нет":
+        bot.send_message(chat_id=message.chat.id, text="Пидора ответ", reply_to_message_id=message.id)
+    elif message.text.lower() == "da":
+        bot.send_message(chat_id=message.chat.id, text="Pizda", reply_to_message_id=message.id)
+    elif message.text.lower() == "net":
+        bot.send_message(chat_id=message.chat.id, text="Pidora otvet", reply_to_message_id=message.id)
+    elif message.text.lower() == "lf":
+        bot.send_message(chat_id=message.chat.id, text="Gbplf", reply_to_message_id=message.id)
+    elif message.text.lower() == "ytn":
+        bot.send_message(chat_id=message.chat.id, text="Gbljhf jndtn", reply_to_message_id=message.id)
+
+
+bot.polling(skip_pending=True)
